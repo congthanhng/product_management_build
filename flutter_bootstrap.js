4cb23d8cbbe4e7f584153b816d92cbf0f9644dfc
Kong <15138747+congthanhng@users.noreply.github.com>
2024-08-27 21:12:10 +0700

    release 1.2.3 + 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3184018464"
+    serviceWorkerVersion: "347133075"
   }
 });
